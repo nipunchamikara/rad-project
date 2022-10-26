7c594f8659674da20c003e2129f919e833c8ff3a
Naveen Dharmathunga <dasheenaveen@outlook.com>
2022-10-26 17:52:04 +0530

    Bug fixed, integraded reminders to the Home page

---
+++ frontend/src/components/Reminders/reminder.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, InputGroup, Form } from "react-bootstrap";
+import EventList from "./event-list";
+import moment from "moment/moment";
+
+function ReminderLayout() {
+  const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+
+  return (
+    <div className="container-fluid layout-container">
+      {/* Body Header */}
+      <div class="container-fluid p-3 mb-4 bg-primary text-white text-center">
+        <h2>Reminders</h2>
+      </div>
+      {/* Body Header */}
+      <div className="container">
+        <Row className="mb-3">
+          <Col>
+            <Link to="/">Create Event</Link>
+          </Col>
+          <Col className="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-7">
+            <Form>
+              <InputGroup>
+                <span className="input-group-text" id="current-date">
+                  Date
+                </span>
+                <Form.Control type="date" value={date} onChange={setDate} />
+              </InputGroup>
+            </Form>
+          </Col>
+        </Row>
+        <EventList />
+      </div>
+    </div>
+  )
+}
+
+export default ReminderLayout;
